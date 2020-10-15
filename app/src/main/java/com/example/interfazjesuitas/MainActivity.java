2886dd628620a6210e0c2725b13eb8c76c5c092e
MrPemu <daw09.2019@gmail.com>
2020-10-15 18:50:52 +0200

    Entrega Obligatoria 1.1 Diseño Interfaz Jesuitas

---
+++ app/src/main/java/com/example/interfazjesuitas/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.interfazjesuitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void megusta(View view) {
+        Toast toast = Toast.makeText(this, R.string.megustaRespuesta,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
